3c6bc07162d36e660004911496c8aad044ce2861
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2019-05-14 11:19:22 +0200

    Add few options to run visulizers

--- sn_tools/sn_visu.py
+++ sn_tools/sn_visu.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib.animation as manimation
+import matplotlib.animation as anim
 from descartes.patch import PolygonPatch
 from shapely.geometry import MultiPolygon
 from shapely.ops import unary_union
@@ -9,9 +10,10 @@ from sn_tools.sn_obs import LSSTPointing, renameFields
 import time
 import itertools
 
+
 def area(polylist):
     """Estimate area of a set of polygons (without overlaps)
-        
+
     Parameters
     -----------
     polylist: list
@@ -22,89 +24,106 @@ def area(polylist):
     area: float
      area corresponding to this set of polygons.
     """
-    
+
     bigpoly = unary_union(MultiPolygon(polylist))
     return bigpoly.area
 
+
 class SnapNight:
-    def __init__(self, dbDir, dbName, nights=1,saveFig=False):
- 
+    def __init__(self, dbDir, dbName, nights=1, saveFig=False, areaTime=False, realTime=False):
+
         self.dbName = dbName
         self.saveFig = saveFig
+        self.areaTime = areaTime
+        self.realTime = realTime
 
-        #Loading observations
+        # Loading observations
         obs = np.load('{}/{}.npy'.format(dbDir, dbName))
         obs = renameFields(obs)
         obs.sort(order='observationStartMJD')
 
-        #Select observations
+        # Select observations
         idx = obs['night'] <= nights
         obs = obs[idx]
 
-        # Prepare the frame
-        self.fig = plt.figure()
-        self.ax = self.fig.gca()
-        self.ax2 = self.fig.add_axes([0.71, 0.675, 0.20, 0.2])
-        
-        P1 = self.fig.add_subplot(1, 1, 1)
-        self.fig.subplots_adjust(right=0.7)
-
-        self.fig.canvas.draw()
-        self.plotParams()
-        self.customize()
-
-        for night in range(np.min(obs['night']),np.max(obs['night'])):
+        for night in range(np.min(obs['night']), np.max(obs['night'])):
             idx = obs['night'] == night
             obs_disp = obs[idx]
+            self.frame()
             mjd0 = np.min(obs_disp['observationStartMJD'])
             polylist = []
             polyarea = []
             mjds = obs_disp['observationStartMJD']-mjd0
             nchanges = len(list(itertools.groupby(obs_disp['filter'])))-1
-            self.fig.suptitle('night {} - filter changes: {}'.format(night,nchanges))
-            print('ruunin on',len(obs_disp))
+            self.fig.suptitle(
+                'night {} - filter changes: {}'.format(night, nchanges))
+
+            # area observed versus time
             for val in obs_disp:
                 pointing = LSSTPointing(val['fieldRA'], val['fieldDec'])
                 p = PolygonPatch(
                     pointing, facecolor=self.colors[val['filter']], edgecolor=self.colors[val['filter']])
                 self.ax.add_patch(p)
-                polylist.append(pointing)
-                polyarea.append(area(polylist))
-            # area observed versus time
-            self.ax2.plot(24.*mjds,polyarea,'b.')
+                # area observed versus time
+                if self.areaTime:
+                    polylist.append(pointing)
+                    polyarea.append(area(polylist))
+
+            if self.areaTime:
+                self.ax2.plot(24.*mjds, polyarea, 'b.')
+            if self.realTime:
+                plt.draw()
+                plt.pause(1.)
+                plt.close()
             if saveFig:
-                plt.savefig('{}_night_{}.png'.format(self.dbName,night))
-            print('end of night',night)
+                plt.savefig('{}_night_{}.png'.format(self.dbName, night))
             self.ax.clear()
-            self.ax2.clear()
+            if self.areaTime:
+                self.ax2.clear()
+
+    def frame(self):
+        # Prepare the frame
+        self.plotParams()
+        self.fig = plt.figure()
+        self.ax = self.fig.gca()
+        adjr = 0.85
+        if self.areaTime:
+            self.ax2 = self.fig.add_axes([0.71, 0.675, 0.20, 0.2])
+            adjr = 0.7
+        P1 = self.fig.add_subplot(1, 1, 1)
+        self.fig.subplots_adjust(right=adjr)
+
+        self.fig.canvas.draw()
+
+        self.customize()
 
     def plotParams(self):
         """Define general parameters for display
         """
 
-        #display parameters
+        # display parameters
         plt.rcParams['xtick.labelsize'] = 12
         plt.rcParams['ytick.labelsize'] = 12
         plt.rcParams['axes.titlesize'] = 12
         plt.rcParams['axes.labelsize'] = 12
         plt.rcParams['legend.fontsize'] = 12
         plt.rcParams['legend.facecolor'] = 'w'
-        plt.rcParams['figure.figsize'] = (11,6)
+        plt.rcParams['figure.figsize'] = (11, 6)
         plt.rcParams['figure.titlesize'] = 12
         # switch on if you want dark background
-        #plt.style.use('dark_background')
+        # plt.style.use('dark_background')
         self.colors = dict(zip('ugrizy', ['b', 'c', 'g', 'y', 'r', 'm']))
 
     def customize(self):
         """Define axis parameters for display
         """
-
-        self.ax2.set_xlabel('Time [hour]',size=8)
-        self.ax2.set_ylabel('area [deg2]',size=8)
-        self.ax2.xaxis.set_tick_params(labelsize=8)
-        self.ax2.yaxis.set_tick_params(labelsize=8)
-        self.ax2.yaxis.tick_right()
-        self.ax2.yaxis.set_label_position("right")
+        if self.areaTime:
+            self.ax2.set_xlabel('Time [hour]', size=8)
+            self.ax2.set_ylabel('area [deg2]', size=8)
+            self.ax2.xaxis.set_tick_params(labelsize=8)
+            self.ax2.yaxis.set_tick_params(labelsize=8)
+            self.ax2.yaxis.tick_right()
+            self.ax2.yaxis.set_label_position("right")
 
         self.ax.set_xlabel('Ra [deg]')
         self.ax.set_ylabel('Dec [deg]')
@@ -113,13 +132,14 @@ class SnapNight:
 
         colorfilters = []
         for band in self.colors.keys():
-            colorfilters.append(mpatches.Patch(color=self.colors[band], label='{}'.format(band)))
-        plt.legend(handles=colorfilters, loc='upper left',bbox_to_anchor=(1.,0.5))       
-
+            colorfilters.append(mpatches.Patch(
+                color=self.colors[band], label='{}'.format(band)))
+        plt.legend(handles=colorfilters, loc='upper left',
+                   bbox_to_anchor=(1., 0.5))
 
 
 class CadenceMovie:
-    def __init__(self, dbDir, dbName, nights=1,title='', total=600, sub=100, fps=24, saveMovie=False, realTime=False, saveFig=False):
+    def __init__(self, dbDir, dbName, nights=1, title='', total=600, sub=100, fps=24, saveMovie=False, realTime=False, saveFig=False, areaTime=False):
         """ Display obs footprint vs time
 
         Parameters
@@ -135,13 +155,13 @@ class CadenceMovie:
          title of the movie
          default : ''
         total: int,opt
-          
+
          default: 600
         sub: int, opt
-         
+
          default: 100
         fps: int, opt
-         
+
          default: 24
         saveMovie: bool, opt
          save the movie as mp4 file
@@ -152,56 +172,61 @@ class CadenceMovie:
         saveFig: bool, opt
          save fig at the end of each night
          default: false
+        areaTime: bool, opt
+         draw observed area vs time in an embedded histo
+         default: False
         """
 
-        self.realTime=realTime
+        self.realTime = realTime
         self.plotParams()
         self.dbName = dbName
         self.saveFig = saveFig
-
-        #Loading observations
+        self.areaTime = areaTime
+        # Loading observations
         obs = np.load('{}/{}.npy'.format(dbDir, dbName))
         obs = renameFields(obs)
         obs.sort(order='observationStartMJD')
 
-        #Select observations
+        # prepare frame
+        fig = plt.figure()
+        ax = fig.gca()
+        adjr = 0.9
+        ax2 = None
+        if self.areaTime:
+            ax2 = self.fig.add_axes([0.75, 0.675, 0.20, 0.2])
+            adjr = 0.74
+        P1 = fig.add_subplot(1, 1, 1)
+        fig.subplots_adjust(right=adjr)
+        fig.canvas.draw()
+        self.customize(ax, ax2)
+
+        # Select observations
         idx = obs['night'] <= nights
         obs = obs[idx]
 
-        # Prepare the frame
-        self.fig = plt.figure()
-        self.ax = self.fig.gca()
-        self.ax2 = self.fig.add_axes([0.75, 0.675, 0.20, 0.2])
-        
-        P1 = self.fig.add_subplot(1, 1, 1)
-        self.fig.subplots_adjust(right=0.74)
-    
-        self.fig.canvas.draw()
-
-        self.customize()
-
-        self.polylist=[]
+        self.polylist = []
         if saveMovie:
             # Warning : to save the movie ffmpeg needs to be installed!
             print(manimation.writers.list())
             FFMpegWriter = manimation.writers['ffmpeg']
             metadata = dict(title=title, artist='Matplotlib',
                             comment=title)
-            writer = FFMpegWriter(fps=fps, metadata=metadata, bitrate=6000)
+            writer = FFMpegWriter(
+                fps=fps, metadata=metadata, bitrate=6000)
+            writer = anim.FFMpegWriter(fps=30, codec='hevc')
             Name_mp4 = title
             with writer.saving(fig, Name_mp4, 250):
-                self.loopObs(obs,writer=writer)
+                self.loopObs(obs, fig, ax, ax2, writer=writer)
         else:
-            self.loopObs(obs)
+            self.loopObs(obs, fig, ax, ax2)
 
-
-    def showObs(self,obs, nchanges, area, mjd0):
+    def showObs(self, obs, nchanges, area, mjd0, fig, ax, ax2):
         """ Display observation (RA, Dec) as time (MJD) evolves.
 
         Parameters
         ----------
         obs: array
-         array of observation 
+         array of observation
         nchanges: int
          number of filter changes
         area: float
@@ -215,19 +240,20 @@ class CadenceMovie:
         mjd = obs['observationStartMJD']
         night = obs['night']
 
-        self.fig.suptitle('night {} - MJD {} \n filter changes: {}'.format(night, np.round(mjd, 3),nchanges))
+        fig.suptitle(
+            'night {} - MJD {} \n filter changes: {}'.format(night, np.round(mjd, 3), nchanges))
 
         # LSST focal plane corresponding to this pointing
         pointing = LSSTPointing(obs['fieldRA'], obs['fieldDec'])
         p = PolygonPatch(
             pointing, facecolor=self.colors[obs['filter']], edgecolor=self.colors[obs['filter']])
-        self.ax.add_patch(p)
-    
-        # area observed versus time
-        self.ax2.plot([24.*(mjd-mjd0)],[area],'b.')
+        ax.add_patch(p)
 
-    
-    def loopObs(self, obs, writer=None):
+        # area observed versus time
+        if self.areaTime:
+            ax2.plot([24.*(mjd-mjd0)], [area], 'b.')
+
+    def loopObs(self, obs, fig, ax, ax2, writer=None):
         """Loop on observations
 
         Parameters
@@ -235,71 +261,100 @@ class CadenceMovie:
         obs: array
          array of observations
         writer: bool, opt
-         write movie on a file 
+         write movie on a file
          default: None
         """
 
-        for night in range(np.min(obs['night']),np.max(obs['night'])):
+        for night in range(np.min(obs['night']), np.max(obs['night'])):
             idx = obs['night'] == night
             obs_disp = obs[idx]
             mjd0 = np.min(obs_disp['observationStartMJD'])
-            
             if len(obs_disp) > 0:
                 for k in range(len(obs_disp)):
                     # number of filter changes up to now
                     sel = obs_disp[:k]
                     nchanges = len(list(itertools.groupby(sel['filter'])))-1
                     # show observations
-                    self.polylist.append(LSSTPointing(obs_disp[k]['fieldRA'],obs_disp[k]['fieldDec']))
-                    self.showObs(obs_disp[k],nchanges,area(self.polylist),mjd0)
-                    self.fig.canvas.flush_events()
-                    if writer is not None:
+                    if self.areaTime:
+                        self.polylist.append(LSSTPointing(
+                            obs_disp[k]['fieldRA'], obs_disp[k]['fieldDec']))
+                        self.showObs(obs_disp[k], nchanges,
+                                     area(self.polylist), mjd0, fig, ax, ax2)
+                    else:
+                        self.showObs(obs_disp[k], nchanges,
+                                     0., mjd0, fig, ax, ax2)
+                    fig.canvas.flush_events()
+                    if writer:
+                        print('grabing frame', k)
                         writer.grab_frame()
             if self.saveFig:
-                plt.savefig('{}_night_{}.png'.format(self.dbName,night))
+                plt.savefig('{}_night_{}.png'.format(self.dbName, night))
             # clear before next night
+            ax.clear()
+            if self.areaTime:
+                ax2.clear()
+            self.customize(ax, ax2)
+            self.polylist = []
+            """
             self.ax.clear()
-            self.ax2.clear()
-            self.polylist=[]
-
- 
+            if self.areaTime:
+                self.ax2.clear()
+            self.customize()
+            self.polylist = []
+            """
 
     def plotParams(self):
         """Define general parameters for display
         """
 
-        #display parameters
+        # display parameters
         if self.realTime:
-            plt.ion() #real-time mode
+            plt.ion()  # real-time mode
         plt.rcParams['xtick.labelsize'] = 12
         plt.rcParams['ytick.labelsize'] = 12
         plt.rcParams['axes.titlesize'] = 12
         plt.rcParams['axes.labelsize'] = 12
         plt.rcParams['legend.fontsize'] = 12
         plt.rcParams['legend.facecolor'] = 'w'
-        plt.rcParams['figure.figsize'] = (11,6)
+        plt.rcParams['figure.figsize'] = (11, 6)
         plt.rcParams['figure.titlesize'] = 12
         # switch on if you want dark background
-        #plt.style.use('dark_background')
+        # plt.style.use('dark_background')
         self.colors = dict(zip('ugrizy', ['b', 'c', 'g', 'y', 'r', 'm']))
 
-    def customize(self):
+    def customize(self, ax, ax2):
         """Define axis parameters for display
         """
-
-        self.ax2.set_xlabel('Time [hour]',size=8)
-        self.ax2.set_ylabel('area [deg2]',size=8)
-        self.ax2.xaxis.set_tick_params(labelsize=8)
-        self.ax2.yaxis.set_tick_params(labelsize=8)
-        self.ax2.yaxis.tick_right()
-        self.ax2.yaxis.set_label_position("right")
+        """
+        if self.areaTime:
+            self.ax2.set_xlabel('Time [hour]', size=8)
+            self.ax2.set_ylabel('area [deg2]', size=8)
+            self.ax2.xaxis.set_tick_params(labelsize=8)
+            self.ax2.yaxis.set_tick_params(labelsize=8)
+            self.ax2.yaxis.tick_right()
+            self.ax2.yaxis.set_label_position("right")
 
         self.ax.set_xlabel('Ra [deg]')
         self.ax.set_ylabel('Dec [deg]')
         self.ax.set_xlim(0, 360.)
         self.ax.set_ylim(-90., 10.)
+        """
+        if self.areaTime:
+            ax2.set_xlabel('Time [hour]', size=8)
+            ax2.set_ylabel('area [deg2]', size=8)
+            ax2.xaxis.set_tick_params(labelsize=8)
+            ax2.yaxis.set_tick_params(labelsize=8)
+            ax2.yaxis.tick_right()
+            ax2.yaxis.set_label_position("right")
+
+        ax.set_xlabel('Ra [deg]')
+        ax.set_ylabel('Dec [deg]')
+        ax.set_xlim(0, 360.)
+        ax.set_ylim(-90., 10.)
 
         colorfilters = []
         for band in self.colors.keys():
-            colorfilters.append(mpatches.Patch(color=self.colors[band], label='{}'.format(band)))
-        plt.legend(handles=colorfilters, loc='upper left',bbox_to_anchor=(1.,0.5))
+            colorfilters.append(mpatches.Patch(
+                color=self.colors[band], label='{}'.format(band)))
+        plt.legend(handles=colorfilters, loc='upper left',
+                   bbox_to_anchor=(1., 0.5))
