ce9a094a9f421d77cc4654e1d2016ba621777b8d
pgris <gris@clermont.in2p3.fr>
2019-06-13 17:35:55 +0200

    Add a new class in sn_utils: X0_norm. It is use to estimate the X0 value of a type Ia supernova depending on (mb,x1,color).

--- sn_tools/sn_utils.py
+++ sn_tools/sn_utils.py
@@ -3,7 +3,10 @@ from sn_tools.sn_rate import SN_Rate
 import os
 import numpy.lib.recfunctions as rf
 from astropy.table import Table
-
+from sn_tools.sn_throughputs import Throughputs
+from scipy import interpolate, integrate
+from lsst.sims.photUtils import Sed, PhotometricParameters,Bandpass, Sed
+import sncosmo
 
 class GenerateSample:
     """ Generates a sample of parameters for simulation
@@ -430,3 +433,276 @@ class Make_Files_for_Cadence_Metric:
         # print(restot)
         np.save('Li_'+self.simulator_name+'_'+str(x1) +
                 '_'+str(color)+'.npy', np.copy(restot))
+
+class X0_norm:
+    """ X0 estimations
+         to be used as input for SN input params
+         for simulation
+
+        Parameters
+        ----------
+        salt2Dir: str, opt
+          directory where to find SALT2 ref. files (default:'SALT2_Files')
+        model: str, opt
+          model use for SN (default:'salt2-extended')
+        version: str, opt
+          version used (default:'1.0')
+        absmag: float, opt 
+          absolute mag for the supernova (default:-19.0906)
+        outfile: str,opt
+         name/location of the output file (default:'reference_files/X0_norm.npy')
+
+        Returns
+        --------
+        None
+        The output file contains a numpy array with the following fields:
+         x1: float, stretch
+         color: float, color
+         flux_10pc: float, flux at 10pc
+         x0_norm: float, x0 value
+    """
+    def __init__(self, salt2Dir='SALT2_Files',model='salt2-extended', version='1.0', absmag= -19.0906,outfile='reference_files/X0_norm.npy'):
+
+        self.salt2Dir = salt2Dir
+        self.model = model
+        self.version = version
+        self.absmag = absmag
+
+        if model == 'salt2-extended':
+            model_min = 300.
+            model_max = 180000.
+            wave_min = 3000.
+            wave_max = 11501.
+
+        if model == 'salt2':
+            model_min = 3400.
+            model_max = 11501.
+            wave_min = model_min
+            wave_max = model_max
+
+        self.wave = np.arange(wave_min, wave_max, 1.)
+
+        #estimate flux at 10pc
+        self.flux_10pc()
+        source = sncosmo.get_source(self.model, version=self.version)
+        self.SN = sncosmo.Model(source=source)
+        r = []
+        for x1 in np.arange(-3.,3.,0.01):
+            for color in np.arange(-0.3,0.3,0.01):
+                r.append((x1,color,self.flux_at_10pc,self.X0_norm(x1,color)))
+        tab = np.rec.fromrecords(r,names=['x1','color','flux_10pc','x0_norm'])
+
+        np.save(outfile,tab)
+                
+
+
+    def flux_10pc(self):
+        """ Extimate flux at 10pc
+        using Vega spectrum
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        --------
+        None
+        """
+        name = 'STANDARD'
+        band = 'B'
+        #thedir = os.getenv('SALT2_DIR')
+        thedir = self.salt2Dir
+
+        os.environ[name] = thedir+'/Instruments/Landolt'
+
+        self.trans_standard = Throughputs(through_dir='STANDARD',
+                                     telescope_files=[],
+                                     filter_files=['sb_-41A.dat'],
+                                     atmos=False,
+                                     aerosol=False,
+                                     filterlist=('A'),
+                                     wave_min=3559,
+                                     wave_max=5559)
+
+        mag, spectrum_file = self.getMag(
+            thedir+'/MagSys/VegaBD17-2008-11-28.dat',
+            np.string_(name),
+            np.string_(band))
+
+        sourcewavelen, sourcefnu = self.readSED_fnu(
+            filename=thedir+'/'+spectrum_file)
+        CLIGHT_A_s = 2.99792458e18         # [A/s]
+        HPLANCK = 6.62606896e-27
+
+        sedb = Sed(wavelen=sourcewavelen, flambda=sourcewavelen *
+                   sourcefnu/(CLIGHT_A_s * HPLANCK))
+
+        flux = self.calcInteg(
+            bandpass=self.trans_standard.system['A'],
+            signal=sedb.flambda,
+            wavelen=sedb.wavelen)
+
+        zp = 2.5*np.log10(flux)+mag
+        self.flux_at_10pc = np.power(10., -0.4 * (self.absmag-zp))
+
+
+    def X0_norm(self,x1,color):
+        """ Extimate X0 from flux at 10pc
+        using Vega spectrum
+
+        Parameters
+        ----------
+        x1: float
+         stretch of the supernova
+        color: float
+         color of the supernova
+
+
+        Returns
+        --------
+        x0: float
+          x0 from flux at 10pc
+        """
+
+        
+        self.SN.set(z=0.)
+        self.SN.set(t0=0)
+        self.SN.set(c=color)
+        self.SN.set(x1=x1)
+        self.SN.set(x0=1)
+
+        fluxes = 10.*self.SN.flux(0., self.wave)
+
+        wavelength = self.wave/10.
+        SED_time = Sed(wavelen=wavelength, flambda=fluxes)
+
+        expTime = 30.
+        photParams = PhotometricParameters(nexp=expTime/15.)
+        trans = Bandpass(
+            wavelen=self.trans_standard.system['A'].wavelen/10.,
+            sb=self.trans_standard.system['A'].sb)
+        # number of ADU counts for expTime
+        e_per_sec = SED_time.calcADU(bandpass=trans, photParams=photParams)
+        # e_per_sec = sed.calcADU(bandpass=self.transmission.lsst_atmos[filtre], photParams=photParams)
+        e_per_sec /= expTime/photParams.gain*photParams.effarea
+
+        return self.flux_at_10pc * 1.E-4 / e_per_sec    
+
+    def getMag(self, filename, name, band):
+        """ Get magnitude in filename
+
+        Parameters
+        --------------
+        filename: str
+          name of the file to scan
+        name: str
+           throughtput used
+        band: str
+          band to consider
+
+        Returns
+        ----------
+        mag: float
+         mag
+        spectrum_file: str
+         spectrum file
+
+        """
+        sfile = open(filename, 'rb')
+        spectrum_file = 'unknown'
+        for line in sfile.readlines():
+            if np.string_('SPECTRUM') in line:
+                spectrum_file = line.decode().split(' ')[1].strip()
+            if name in line and band in line:
+                return float(line.decode().split(' ')[2]), spectrum_file
+
+        sfile.close()
+
+    def calcInteg(self, bandpass, signal, wavelen):
+        """ Estimate integral of signal
+        over wavelength using bandpass
+
+        Parameters
+        --------------
+        bandpass: list(float)
+          bandpass
+        signal:  list(float)
+          signal to integrate (flux)
+        wavelength: list(float)
+          wavelength used for integration
+
+        Returns
+        -----------
+        integrated signal (float)
+        """
+
+        fa = interpolate.interp1d(bandpass.wavelen, bandpass.sb)
+        fb = interpolate.interp1d(wavelen, signal)
+
+        min_wave = np.max([np.min(bandpass.wavelen), np.min(wavelen)])
+        max_wave = np.min([np.max(bandpass.wavelen), np.max(wavelen)])
+
+        wavelength_integration_step = 5
+        waves = np.arange(min_wave, max_wave, wavelength_integration_step)
+
+        integrand = fa(waves) * fb(waves)
+
+        range_inf = min_wave
+        range_sup = max_wave
+        n_steps = int((range_sup-range_inf) / wavelength_integration_step)
+
+        x = np.core.function_base.linspace(range_inf, range_sup, n_steps)
+
+        return integrate.simps(integrand, x=waves)
+
+    def readSED_fnu(self, filename, name=None):
+        """
+        Read a file containing [lambda Fnu] (lambda in nm) (Fnu in Jansky).
+        Extracted from sims/photUtils/Sed.py which does not seem to work
+
+        Parameters
+        --------------
+        filename: str
+          name of the file to process
+        name: str,opt
+          default: None
+
+        Returns
+        ----------
+        sourcewavelen: list(float)
+         wavelength with lambda in nm
+        sourcefnu: list(float)
+         signal with Fnu in Jansky
+        """
+        # Try to open the data file.
+        try:
+            if filename.endswith('.gz'):
+                f = gzip.open(filename, 'rt')
+            else:
+                f = open(filename, 'r')
+        # if the above fails, look for the file with and without the gz
+        except IOError:
+            try:
+                if filename.endswith(".gz"):
+                    f = open(filename[:-3], 'r')
+                else:
+                    f = gzip.open(filename+".gz", 'rt')
+            except IOError:
+                raise IOError(
+                    "The throughput file %s does not exist" % (filename))
+        # Read source SED from file
+        # lambda, fnu should be first two columns in the file.
+        # lambda should be in nm and fnu should be in Jansky.
+        sourcewavelen = []
+        sourcefnu = []
+        for line in f:
+            if line.startswith("#"):
+                continue
+            values = line.split()
+            sourcewavelen.append(float(values[0]))
+            sourcefnu.append(float(values[1]))
+        f.close()
+        # Convert to numpy arrays.
+        sourcewavelen = np.array(sourcewavelen)
+        sourcefnu = np.array(sourcefnu)
+        return sourcewavelen, sourcefnu
