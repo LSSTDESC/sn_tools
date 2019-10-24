2c8642f8fe9cf5583583b8da61ddc875f70a3918
pgris <gris@clermont.in2p3.fr>
2019-10-24 15:15:26 +0200

    Add class Read_Sqlite to read simulation database from LSST scheduler and transform it as a numpy array

--- sn_tools/sn_io.py
+++ sn_tools/sn_io.py
@@ -7,6 +7,9 @@ import glob
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 from scipy.interpolate import interp1d
+import numpy.lib.recfunctions as rf
+import sqlite3
+import logging
 
 def geth5Data(name,thedir):
     
@@ -115,8 +118,17 @@ def convert_save(inputDir,dbName,metricName,outDir,fieldType='WFD',objtype='pand
     print('files',fileNames)
     tab = convert_to_npy(fileNames,objtype=objtype)
     
+    #Be carefull here...
+    # healpixID is sometimes healpixId
+    # so add healpixID if not present
+
+    healpixID = 'healpixID'
+    if 'healpixId' in tab.dtype.names:
+        healpixID = 'healpixId'
+
+
     if unique:
-        u, indices = np.unique(tab['healpixID'], return_index=True)
+        u, indices = np.unique(tab[healpixID], return_index=True)
         tab = np.copy(tab[indices])
 
     if remove_galactic:
@@ -174,3 +186,85 @@ def remove_galactic_area(tab):
     sela = np.concatenate((sela,selp))
     
     return sela
+
+
+class Read_Sqlite:
+    
+    def __init__(self, dbfile, **sel):
+        """
+        """
+        self.dbfile = dbfile
+        conn = sqlite3.connect(dbfile)
+        self.cur = conn.cursor()
+        # get the table list
+        self.cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        self.tables = self.cur.fetchall()
+        self.sql = self.sql_selection(**sel)
+        #        self.data = self.groom_data(self.get_data(sql))
+        
+    def sql_selection(self, **sel):
+        sql = ''
+        if 'year' in sel and sel['year'] is not None:
+            y = sel['year']
+            sql += 'night > %i and night < %i' % ((y-1)*365.25, y*365.25)
+        if 'mjd_min' in sel and sel['mjd_min'] is not None:
+            if len(sql) > 0: sql += ' and '
+            sql += 'observationStartMJD > %f' % sel['mjd_min']
+        if 'mjd_max' in sel and sel['mjd_max'] is not None:
+            if len(sql) > 0: sql += ' and '
+            sql += 'observationStartMJD < %f' % sel['mjd_max']            
+        if 'proposalId' in sel and sel['proposalId'] is not None:
+            if len(sql) > 0: sql += ' and '
+            sql += 'proposalId=%d' % sel['proposalId']
+        return sql
+        
+    def get_data(self, cols=None, sql=None, to_degrees=False, new_col_names=None):
+        """
+        Get the contents of the SQL database dumped into a numpy rec array
+        """
+        sql_request = 'SELECT '
+        if cols is None:
+            sql_request += ' * '            
+            self.cur.execute('PRAGMA TABLE_INFO(SummaryAllProps)')
+            r = self.cur.fetchall()
+            cols = [c[1] for c in r]
+        else:
+            sql_request += ','.join(cols)
+            
+        sql_request += 'FROM SummaryAllProps'
+        if sql is not None and len(sql) > 0:
+            sql_request += ' WHERE ' + sql
+        sql_request += ';'
+        
+        logging.info('fetching data from db')
+        logging.info('request: %s' % sql_request)
+        self.cur.execute(sql_request)
+        rows = self.cur.fetchall()
+        logging.info('done. %d entries' % len(rows))
+
+        logging.info('converting dump into numpy array')
+        colnames = [str(c) for c in cols]
+        d = np.rec.fromrecords(rows, names=colnames)
+        """
+        logging.info('update kAtm values')
+        katm = np.zeros(len(d), dtype='float64')        
+        logging.info('extending output array')
+        d = croaks.rec_append_fields(d, data=[katm], names=['kAtm'])
+        for b in kAtm.keys():
+            d['kAtm'][d['filter'] == b] = kAtm[b]
+        """
+        logging.info('done.')
+
+        if to_degrees:
+            d['fieldRA'] *= (180. / np.pi)
+            d['fieldDec'] *= (180. / np.pi)
+
+        if new_col_names is not None:
+            self.update_col_names(d, new_col_names)
+            
+        return d
+
+    def update_col_names(self, d, new_col_names):
+        names = list(d.dtype.names)
+        d.dtype.names = [new_col_names[n] if n in new_col_names else n for n in d.dtype.names]
+        return d
