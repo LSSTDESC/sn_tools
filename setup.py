f5c7812acde3b2ee0b2f80b9a324b90d4d1b0382
pgris <gris@clermont.in2p3.fr>
2020-07-15 11:33:15 +0200

    try to define correct versioning

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
 
+# get the version here
+pkg_vars  = {}
+
+with open("version.py") as fp:
+    exec(fp.read(), pkg_vars)
+
 setup(
     name='sn_tools',
-    version=__version__,
+    version= pkg_vars['__version__'],
     description='Set of tools used to run SN pipeline',
     url='http://github.com/lsstdesc/sn_tools',
     author='Philippe Gris',
     author_email='philippe.gris@clermont.in2p3.fr',
     license='BSD',
-    packages=['sn_tools'],
+    packages= ['sn_tools'],
     python_requires='>=3.5',
     zip_safe=False
 )
