10b1f8a7e116b828c98ee2aad58217c52a3f3050
Philippe Gris <gris@clermont.in2p3.fr>
2020-10-15 14:08:11 +0200

    add two functions in sn_io to be used when generating parser option in a dynamic way

--- sn_tools/sn_io.py
+++ sn_tools/sn_io.py
@@ -841,3 +841,86 @@ def make_dict_from_optparse(thedict):
         params = recursive_merge(params, thedict)
     
     return params
+
+def decrypt_parser(parser):
+    """
+    Method to decrypt the parser help
+
+    Parameters
+    ---------------
+    parser: optparse parser
+
+
+    Returns
+    ----------
+    dict with decrypted infos
+
+    """
+    
+    file_name = 'help_script.txt'
+    file_object  = open(file_name,'w')
+    parser.print_help(file_object)
+    file_object.close()
+
+    file = open(file_name, 'r') 
+    line = file.read().splitlines()
+
+    params = {}
+    for i,ll in enumerate(line):
+        lolo = ' '.join(ll.split(' ')).split()
+        if lolo and lolo[0][:2]=='--':
+            key = lolo[0].split('--')[1]
+            key = key.split('=')
+            params[key[0]]= (key[1].split('/')[0],key[1].split('/')[1])
+
+    return params
+
+def make_dict_old(thedict,key,what,val):
+    """
+    Method to append to a dict infos
+    The goal is in fine to create a yaml file
+
+    Parameters
+    --------------
+    thedict: dict
+      the dict to append to
+    key: str
+     key used to append
+    what: (str,str)
+      name and type
+    val: str
+      value
+
+    Returns
+    ----------
+    thedict: dict
+      resulting dict
+
+    """
+      
+    
+    keym = key.split('_')[0]
+    keys = ''
+    if '_' in key:
+        keys = '_'.join(key.split('_')[1:])
+
+    names = what[0].split(',')
+    dtypes = what[1].split(',')
+    val = val.split(',')
+    
+    valb = [val[i] if dtypes[i]=='str' else eval('{}({})'.format(dtypes[i],val[i]))  for i in range(len(dtypes))]
+    
+    if keys=='':
+        if names[0] != 'value':
+            thedict[keym]=dict(zip(names,valb))
+        else:
+            thedict[keym]=valb[0]
+    else:
+        if keym not in thedict.keys():
+            thedict[keym] = {}
+        if names[0] != 'value':
+            thedict[keym][keys]=dict(zip(names,valb))  
+        else:
+            thedict[keym][keys]=valb[0] 
+            
+    return thedict
