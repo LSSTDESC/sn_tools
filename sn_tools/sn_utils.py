93c2ade9b0b16e92ca4dd495d8f16de69591c9b4
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2021-03-15 10:37:08 +0100

    add a multiprocessing method that is quite general

--- sn_tools/sn_utils.py
+++ sn_tools/sn_utils.py
@@ -16,6 +16,121 @@ import pandas as pd
 from scipy.interpolate import RegularGridInterpolator
 import multiprocessing
 import pprint
+import operator
+
+
+def multiproc(data, params, func, nproc):
+    """
+    Function to perform multiprocessing
+
+    Parameters
+    ---------------
+    data: array
+      data to process
+    params: dict
+      fixed parameters of func
+    func: function
+      function to apply for multiprocessing
+    nproc: int
+      number of processes
+
+    """
+    nproc = min([len(data), nproc])
+    print('nproc')
+    # multiprocessing parameters
+    nz = len(data)
+    t = np.linspace(0, nz, nproc+1, dtype='int')
+    # print('multi', nz, t)
+    result_queue = multiprocessing.Queue()
+
+    procs = [multiprocessing.Process(name='Subprocess-'+str(j), target=func,
+                                     args=(data[t[j]:t[j+1]], params, j, result_queue))
+             for j in range(nproc)]
+
+    for p in procs:
+        p.start()
+
+    resultdict = {}
+    # get the results in a dict
+
+    for i in range(nproc):
+        resultdict.update(result_queue.get())
+
+    for p in multiprocessing.active_children():
+        p.join()
+
+    restot = gather_results(resultdict)
+
+    return restot
+
+
+def gather_results(resultdict):
+    """
+    Function to gather results of a directory
+
+    Parameters
+    ----------------
+    resultdict: dict
+      dictory of data
+
+    Returns
+    ----------
+    gathered results. The type is determined from resultdict.
+    Supported types: pd.core.frame.DataFrame, Table, np.ndarray,
+    np.recarray, int
+
+    """
+    supported_types = ['pd.core.frame.DataFrame', 'Table', 'np.ndarray',
+                       'np.recarray', 'int']
+
+    # get outputtype here
+    first_value = None
+    for key, vals in resultdict.items():
+        if vals is not None:
+            first_value = vals
+            break
+
+    restot = None
+    if first_value is None:
+        return restot
+
+    if isinstance(first_value, pd.core.frame.DataFrame):
+        restot = pd.DataFrame()
+
+        def concat(a, b):
+            return pd.concat((a, b), sort=False)
+
+    if isinstance(first_value, Table):
+        restot = Table()
+
+        def concat(a, b):
+            return vstack([a, b])
+
+    if isinstance(first_value, np.ndarray) or isinstance(first_value, np.recarray):
+        restot = []
+
+        def concat(a, b):
+            if isinstance(a, list):
+                return b
+            else:
+                return np.concatenate((a, b))
+
+    if isinstance(first_value, int):
+        restot = 0
+
+        def concat(a, b):
+            return operator.add(a, b)
+
+    if restot is None:
+        print('Sorry to bother you but: unknown data type', type(first_value))
+        print('Supported types', supported_types)
+        return restot
+
+    # gather the results
+    for key, vals in resultdict.items():
+        restot = concat(restot, vals)
+
+    return restot
 
 
 class MultiProc:
@@ -694,7 +809,7 @@ class SimuParameters:
             N_SN *= NSN_factor
             N_SN = int(N_SN)
 
-            #print('nsn from rate', zmin, zmax,
+            # print('nsn from rate', zmin, zmax,
             #      duration, self.area, self.min_rf_phase_qual, self.max_rf_phase_qual, N_SN, NSN_factor)
 
             if N_SN < 0.5:
@@ -2423,11 +2538,11 @@ class LoadDust:
         tab['ebvofMW'] = tab['ebvofMW'].round(2)
         tab['phase'] = tab['phase'].round(1)
         tab.convert_bytestring_to_unicode()
-        
+
         for b in bands:
             idx = tab['band'] == b
             rec = tab[idx]
-            
+
             phasemin, phasemax, phasestep, nphase = limVals(rec, 'phase')
             zmin, zmax, zstep, nz = limVals(rec, 'z')
             ebvofMWmin, ebvofMWmax, ebvofMWstep, nebvofMW = limVals(
@@ -2441,7 +2556,7 @@ class LoadDust:
                 (rec['ebvofMW'], rec['z'], rec['phase']))
 
             self.dustcorr[b] = {}
-            for vv in ['flux', 'dx0', 'dx1', 'dcolor', 'ddaymax','fluxerr_model']:
+            for vv in ['flux', 'dx0', 'dx1', 'dcolor', 'ddaymax', 'fluxerr_model']:
                 ratio = np.reshape(
                     rec[index]['ratio_{}'.format(vv)], (nphase, nz, nebvofMW))
                 self.dustcorr[b]['ratio_{}'.format(vv)] = RegularGridInterpolator(
@@ -2463,12 +2578,11 @@ class LoadDust:
         """
 
         if len(grp) != len(zvals):
-            print(len(grp),grp.columns)
-        
-        return pd.DataFrame({'test': [0]})
-                                                            
+            print(len(grp), grp.columns)
 
-                                                             
+        return pd.DataFrame({'test': [0]})
+
+
 class Gamma:
     """ 
     Class to estimate gamma parameters
