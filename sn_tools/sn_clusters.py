83442aac507adf2953f25048520385a9d9577b2a
pgris <gris@clermont.in2p3.fr>
2023-12-21 13:07:05 +0100

    extract two functions to make and analyze clusters of data

--- sn_tools/sn_clusters.py
+++ sn_tools/sn_clusters.py
@@ -240,3 +240,136 @@ def getName(df_fields, RA):
     _idx = np.abs(_fields['RA'] - RA).argmin()
 
     return _idx, _fields[_idx]['name']
+
+
+def makeClusters(nclusters, data, x_name, y_name):
+    """
+    Method to identify clusters
+    It uses the KMeans algorithm from scipy
+
+    Parameters
+    ---------------
+
+    nclusters: int
+     number of clusters to find
+    data: numpy array
+      data to process
+    x_name: str
+      x-variable name
+    y_name: str
+      y-variable name
+
+    Returns
+    -----------
+    points: numpy array
+      array of (RA,Dec) of the points
+    y_km: numpy array
+      index of the clusters
+    kmeans.labels_: numpy array
+      kmeans label
+    """
+
+    """
+       r = []
+       for (pixRA, pixDec) in self.data[[self.RA_name,self.Dec_name]]:
+           r.append([pixRA, pixDec])
+
+       points = np.array(r)
+       """
+
+    points = np.array(data[[x_name, y_name]].tolist())
+
+    # create kmeans object
+    kmeans = KMeans(n_clusters=nclusters)
+    # fit kmeans object to data
+    kmeans.fit(points)
+
+    # print location of clusters learned by kmeans object
+    #print('cluster centers', kmeans.cluster_centers_)
+
+    # save new clusters for chart
+    y_km = kmeans.fit_predict(points)
+
+    return points, y_km, kmeans.labels_
+
+
+def anaClusters(nclusters, data, points, clus, labels, x_name, y_name):
+    """
+    Method matching clusters to data
+
+    Parameters
+    ---------------
+    nclusters: int
+     number of clusters to consider
+    data: numpy array
+       data to process
+    points: numpy array
+      array of (RA,Dec) of the points
+    y_km: numpy array
+      index of the clusters
+    kmeans.labels_: numpy array
+      kmeans label
+
+    Returns
+    -----------
+    env: numpy record array
+      summary of cluster infos:
+      clusid, fieldId, RA, Dec, width_RA, width_Dec,
+      area, dbName, fieldName, Nvisits, Nvisits_all,
+      Nvisits_u, Nvisits_g, Nvisits_r, Nvisits_i,
+      Nvisits_z, Nvisits_y
+    dfcluster: pandas df
+      for each data point considered: RA,Dec,fieldName,clusId
+
+    """
+
+    rcluster = pd.DataFrame()
+    dfcluster = pd.DataFrame()
+    datacluster = pd.DataFrame()
+    for io in range(nclusters):
+
+        RA = points[clus == io, 0]
+        Dec = points[clus == io, 1]
+        dfclus = pd.DataFrame({x_name: RA, y_name: Dec})
+        # ax.scatter(RA,Dec, s=10, c=color[io])
+        indx = np.where(labels == io)[0]
+        sel_obs = data[indx]
+        Nvisits = getVisitsBand(sel_obs)
+
+        min_RA = np.min(RA)
+        max_RA = np.max(RA)
+        min_Dec = np.min(Dec)
+        max_Dec = np.max(Dec)
+        mean_RA = np.mean(RA)
+        mean_Dec = np.mean(Dec)
+        area = np.pi*(max_RA-min_RA)*(max_Dec-min_Dec)/4.
+        # idx, fieldName = getName(self.fields, mean_RA)
+
+        datacluster_loc = pd.DataFrame(np.copy(sel_obs))
+        #datacluster_loc.loc[:, 'fieldName'] = fieldName
+        datacluster_loc.loc[:, 'clusId'] = int(io)
+        datacluster_loc.loc[:, x_name] = mean_RA
+        datacluster_loc.loc[:, y_name] = mean_Dec
+        datacluster = pd.concat([datacluster, datacluster_loc], sort=False)
+
+        # dfclus.loc[:, 'fieldName'] = fieldName
+        dfclus.loc[:, 'clusId'] = int(io)
+        dfcluster = pd.concat([dfcluster, dfclus], sort=False)
+
+        rclus = pd.DataFrame(columns=['clusid'])
+        rclus.loc[0] = int(io)
+        rclus.loc[:, x_name] = mean_RA
+        rclus.loc[:, y_name] = mean_Dec
+        rclus.loc[:, 'width_RA'] = max_RA-min_RA
+        rclus.loc[:, 'width_Dec'] = max_Dec-min_Dec
+        rclus.loc[:, 'area'] = area
+        # rclus.loc[:, 'dbName'] = self.dbName
+        # rclus.loc[:, 'fieldName'] = fieldName
+        rclus.loc[:, 'Nvisits'] = int(Nvisits['all'])
+
+        for key, vals in Nvisits.items():
+            rclus.loc[:, 'Nvisits_{}'.format(key)] = int(vals)
+
+        rcluster = pd.concat((rcluster, rclus))
+
+    return rcluster, dfcluster, datacluster
